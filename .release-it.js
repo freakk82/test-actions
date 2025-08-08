99eb09da260ec9702ad9f846f55fcc27baf87c1b
Freakk <ffr3akk@gmail.com>
2025-08-08 16:03:39 +0100

    Release 1.0.1

---
+++ .release-it.js
@@ -0,0 +1,15 @@
+module.exports = {
+  git: {
+    commitMessage: 'Release ${version}',
+    tagName: 'v${version}',
+    requireCommits: true,
+    requireCleanWorkingDir: false,
+  },
+  github: {
+    release: false,
+    releaseName: 'v${version}'
+  },
+  npm: {
+    publish: false
+  }
+}
